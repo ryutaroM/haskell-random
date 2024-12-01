9ab76e85f0df0c8ce418a297f8a3f572af9cc058
ryutaroM <arcticmonkey326@gmail.com>
2024-12-01 13:39:13 +0900

    done up to randnumber sugget

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,12 @@
 module Main (main) where
 
+import Suggest (s)
 import System.Random
 
 main :: IO ()
 main = do
   gen <- getStdGen
   putStrLn $ take 20 (randomRs ('a', 'z') gen)
+  gen' <- newStdGen
+  putStrLn $ take 20 (randomRs ('a', 'z') gen')
+  s
---
+++ src/Suggest.hs
@@ -0,0 +1,21 @@
+module Suggest (s) where
+
+import Control.Monad (when)
+import System.Random (Random (randomR), StdGen, getStdGen)
+
+s :: IO ()
+s = do
+  gen <- getStdGen
+  askForNumber gen
+
+askForNumber :: StdGen -> IO ()
+askForNumber gen = do
+  let (randNumber, newGen) = randomR (1, 10) gen :: (Int, StdGen)
+  putStrLn "Which number in the range from 1 to 10 am I thinking of?"
+  numberString <- getLine
+  when (not $ null numberString) $ do
+    let number = read numberString
+    if randNumber == number
+      then putStrLn "You are correct!"
+      else putStrLn $ "Sorry, it was " ++ show randNumber
+    askForNumber newGen
